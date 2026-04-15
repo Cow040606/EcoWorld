0a32c87cf2f538297de599f907028bf3dede3e7d
Tantran282006 <87117501+Tantran282006@users.noreply.github.com>
2026-04-15 10:51:54 +0700

    a

--- Assets/Script/LocalPortal.cs
+++ Assets/Script/LocalPortal.cs
@@ -7,13 +7,31 @@ public class LocalPortal : NetworkBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Có vật thể chạm vào cổng: " + other.gameObject.name);
+
         if (other.CompareTag("Player"))
         {
+            Debug.Log("Đã xác nhận đây là Player!");
+            
             NetworkCharacterController ncc = other.GetComponent<NetworkCharacterController>();
             
-            if (ncc != null && ncc.HasStateAuthority)
+            if (ncc != null)
             {
-                ncc.Teleport(targetDestination.position);
+                Debug.Log("Đã tìm thấy NetworkCharacterController!");
+                
+                if (ncc.HasStateAuthority)
+                {
+                    Debug.Log("Có quyền Authority, tiến hành Teleport!");
+                    ncc.Teleport(targetDestination.position);
+                }
+                else
+                {
+                    Debug.Log("Không có State Authority, bỏ qua Teleport.");
+                }
+            }
+            else
+            {
+                Debug.Log("Không tìm thấy Component NetworkCharacterController trên Player!");
             }
         }
     }
