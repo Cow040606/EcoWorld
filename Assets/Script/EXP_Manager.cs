2d25bb4811db96d2b915ef183eeadbdf35518a66
docao040606-ux <docao040606@gmail.com>
2026-05-16 19:46:24 +0700

    ...

---
+++ Assets/Script/EXP_Manager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class EXP_Manager : MonoBehaviour
+{
+    public Slider sliderExp;
+    public TextMeshProUGUI ExpText;
+    void Update()
+    {
+        Player_Controller myPlayer = Player_Controller.localPlayer;
+
+        if(sliderExp != null && myPlayer != null) 
+        {
+            sliderExp.value = myPlayer.ExpCurrent / myPlayer.expToLevelUp;
+            ExpText.text = " Lvl " +myPlayer.level.ToString();
+        }
+        else
+        {
+            Debug.LogWarning("Slider hoặc Player_Controller chưa được gán!");
+        }
+    }
+}
